a62191d7cc0bf2ab5994caed357e6792ccc50eae
William Bucher <wjbuhcer3@gmail.com>
2022-12-12 21:55:50 -0500

    working ui with random colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
 import { LedControllerService } from './led-controller.service';
 
@@ -8,9 +8,11 @@ import { LedControllerService } from './led-controller.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  startingColor: number[] = [100, 200, 20];
-  ledCount: number[][] = [];
 
+
+  startingColor: number[] = [100, 200, 20];
+  ledList: number[][] = [];
+  
   constructor(private service: LedControllerService) {
     this.fillGrid();
   }
@@ -18,13 +20,14 @@ export class AppComponent {
   // creates the initial condition of the led matrix
   fillGrid() {
     for (let i = 0 ; i < 100; i++) {
-      this.ledCount[i] = [255, 255, 255];
+      this.ledList[i] = [255, 255, 255];
     }
   }
 
+
   // method that will run when the led is clicked
   ledClick(led: any, i: number) {
-
+    
     // change the color
     // TODO add some way for user to pick color, slider popup?
     led[0] = Math.floor(Math.random() * 255);
